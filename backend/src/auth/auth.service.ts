aa312584632e0c862d4138b7900d93323de13bf9
khalil kraiem <khalilmedkraiem@gmail>
2025-04-26 12:54:10 +0100

    modify seed and schema

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { CreateAuthDto, loginDto } from './dto/create-auth.dto';
+import { UpdateAuthDto } from './dto/update-auth.dto';
+import { PrismaService } from 'nestjs-prisma';
+import * as bcrypt from 'bcrypt'
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async login(dto: loginDto) {
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (user) {
+      
+      if (await bcrypt.compare(dto.password, user.password)) {
+        return user;
+      } else  throw new HttpException('invalid password', HttpStatus.BAD_REQUEST)
+    } else {
+      throw new HttpException('invalid email', HttpStatus.BAD_REQUEST)
+    }
+  }
+
+  findAll() {
+    return `This action returns all auth`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} auth`;
+  }
+
+  update(id: number, updateAuthDto: UpdateAuthDto) {
+    return `This action updates a #${id} auth`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} auth`;
+  }
+}
