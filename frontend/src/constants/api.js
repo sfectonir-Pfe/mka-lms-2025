9d8ba01179b5c6ea511d92b79d6a13e3b7d1ed71
khalil kraiem <khalilmedkraiem@gmail>
2025-07-20 14:03:10 +0100

    apiconstant front / dockercompose backend

--- frontend/src/constants/api.js
+++ frontend/src/constants/api.js
@@ -1,4 +1,4 @@
 import axios from "axios"
 
 const apiUrl= process.env.REACT_APP_URL
-export const api=axios.create({baseURL:"https://htqr4jqc-8000.euw.devtunnels.ms/"})
+export const api=axios.create({baseURL:apiUrl})
